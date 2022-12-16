5b1836116e7421cd3936391fd3708d91868fa317
Abran Rodriguez Jr <abranrodriguezjr@gmail.com>
2022-12-15 18:00:17 -0600

    generate markdown file updated

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,85 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const index = require('../index.js');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = '';
+  if (license === 'MIT') {
+    badge = '![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)'
+  } else if (license === 'Apache 2.o') {
+    badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+  } else if (license === 'GPL v3.0') {
+    badge = '![License](https://img.shields.io/badge/License-GPLv3-blue.svg'
+  } else {
+    badge = ''
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = '';
+  if (licenseLink === 'MIT') {
+    licenseLink = 'https://choosealicense.com/licenses/mit/'
+  } else if (licenseLink === 'Apache 2.0') {
+    licenseLink = 'https://www.apache.org/licenses/LICENSE-2.0'
+  } else if (licenseLink === 'GPL v3.0') {
+    licenseLink = 'https://www.gnu.org/licenses'
+  } else {
+    licenseLink = ""
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = ''
+  if (license === 'None') {
+    licenseSection = ''
+  } else {
+    licenseSection = 'License: ${license}'
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answer) {
+  return `# ${answer.title}
 
+  ## ${renderLicenseSection(answer.license)} ${renderLicenseBadge(answer.license)}
+  ### ${renderLicenseLink(answer.license)}
+
+  ## Table of Contents:
+  ### * [Licensse](#license)
+  ### * [Installation](#installation)
+  ### * [Usage](#usage)
+  ### * [Contributors](#contributors)
+  ### * [Test](#test)
+  ### * [Questions](#questions)
+
+  ## Installation:
+  ### You must install the following for this app to function:
+  ### ${answer.installation}
+
+  ## Usage:
+  ### ${answer.usage}
+
+  ## Contributors:
+  ### ${answer.contributions}
+
+  ## Tests:
+  ### Run the following commands in your terminal to test this app:
+  ### ${answer.test}
+
+  ## Questions: 
+  ### If you have any questions, you may contact me at either
+  ### Github: https://github.com/${answer.questions}
+  ### or
+  ### Email: ${answer.email}
 `;
 }
 
